55c07d29c7553830b64ea4c8c6288df274280db7
tarun <tarun.diligentic@gmail.com>
2024-02-13 12:24:37 +0530

    capital hosting

--- controllers/feed.js
+++ controllers/feed.js
@@ -132,7 +132,7 @@ exports.getPost = (req, res, next) => {
 
 exports.helloPost = (req, res, next) => {
   try{
-    res.status(200).json({ message: "HOSTING succesfull." });
+    res.status(200).json({ message: "HOSTING SUCCESSFULL." });
   }catch(error){
     console.log(error)
     res.status(400).json({ message: "Where are you now !!" });
